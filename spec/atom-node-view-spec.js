6a8bdbc28960e2bdf324024fbb63bb6363432f78
Danny Buonocore <danBuonocore@gmail.com>
2016-10-26 17:27:57 -0400

    Atom generated package

---
+++ spec/atom-node-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomNodeView from '../lib/atom-node-view';
+
+describe('AtomNodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
